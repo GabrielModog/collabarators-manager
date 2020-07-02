d5b26ccafe6acddc5e4fee476de5728466b37da4
GabrielModog <gabrielmodog@gmail.com>
2020-07-02 12:03:45 -0300

    fixes roles

--- Client/src/pages/Roles/index.js
+++ Client/src/pages/Roles/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form } from '../../components/Form';
 import Input from '../../components/Form/Input';
 
@@ -6,11 +6,12 @@ import { Container, Row, SendButton } from '../style';
 // import roleSchema from '../../validators/role'
 
 const Roles = () => {
+	const formRef = useRef(null);
 	return (
 		<Container>
 			<h1>Criar Cargo</h1>
 			<Row>
-				<Form>
+				<Form ref={formRef}>
 					<Row>
 						<Input type="text" name="name" placeholder="Nome do cargo" />
 						<Input type="text" name="slug" placeholder="Slug do cargo" />
